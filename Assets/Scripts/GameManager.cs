6280ea0e4aef3218908a04cdd1de8f25d6970a04
Ana-Camelia <cameliabejan38@yahoo.com>
2022-06-13 13:31:55 +0300

    Update Menu - Connect without HUD

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,13 +3,50 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using Mirror;
+using TMPro;
 
 public class GameManager : NetworkBehaviour
 {
     float gameOverDelay = 3f;
     float nextLevelDelay = 0.05f;
+    [SerializeField] GameObject ConnectPanel;
+    [SerializeField] TMP_InputField addressInput = null;
 
+    #region Main Menu
+    public void OpenConnectPanel()
+    {
+        ConnectPanel.SetActive(true);
+    }
 
+    public void StartServer()
+    {
+        NetworkManager.singleton.StartServer();
+        NetworkChangeScene("Level 1");
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+    #endregion
+
+    #region Connect Panel
+    public void StartGame()
+    {
+        string address = addressInput.text;
+
+        NetworkManager.singleton.networkAddress = address;
+        NetworkManager.singleton.StartClient();
+
+    }
+
+    public void CloseConnectPanel()
+    {
+        ConnectPanel.SetActive(false);
+    }
+
+    #endregion
+
+    #region Changing Scenes
     [Server]
     public void LoadNextLevel()
     {
@@ -30,13 +67,9 @@ public class GameManager : NetworkBehaviour
         if (NetworkServer.isLoadingScene) return;
         NetworkManager.singleton.ServerChangeScene(sceneName);
     }
+    #endregion
 
-
-
-    public void StartGame()
-    {
-        SceneManager.LoadScene("Level 1");
-    }
+    #region Other
     public void LoadStartMenu()
     {
         SceneManager.LoadScene(0);
@@ -53,9 +86,13 @@ public class GameManager : NetworkBehaviour
         yield return new WaitForSeconds(gameOverDelay);
         SceneManager.LoadScene("GameOver");
     }
+    #endregion
 
-    public void ExitGame()
-    {
-        Application.Quit();
-    }
+
+
+
+
+
+
+
 }
